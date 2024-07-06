714e6fe8c0d8cf7abf66f3dd6a091ae24f717a72
JavierMiz <javiermizarevalo@gmail.com>
2024-07-05 21:57:15 -0500

    chore: Update layout and header in Astro files, add Close and Menu icons, and update dependencies

---
+++ *.d.ts
@@ -0,0 +1,4 @@
+type NAV_ITEM = {
+  name: string;
+  href: string;
+};
---
+++ src/constants/layout.ts
@@ -0,0 +1,12 @@
+export const HEADER = {
+  title: 'Javier miz',
+  menu_items: [
+    { name: 'Item 1', href: '/' },
+    { name: 'Item 2', href: '/' },
+    { name: 'Item 3', href: '/' },
+  ],
+};
+
+export const FOOTER = {
+  title: 'Javier miz',
+};
