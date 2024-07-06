c1affacdd89d54cc413716495468a9fc049fbc51
JavierMiz <javiermizarevalo@gmail.com>
2024-07-05 19:21:57 -0500

    chore: Update layout and header in Astro files, and update dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '3rem',
+      },
+    },
+    extend: {
+      transitionDuration: {
+        DEFAULT: '500ms',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      colors: {
+        primary: '#fff064',
+      },
+      fontFamily: {
+        primary: ['Saira', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
