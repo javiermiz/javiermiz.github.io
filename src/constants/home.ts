5af1a22aa7a15fafd6577f5fba13d322c8af255e
JavierMiz <javiermizarevalo@gmail.com>
2024-07-24 12:50:56 -0500

    refactor: Update portfolio layout in Portfolio.astro

--- src/constants/home.ts
+++ src/constants/home.ts
@@ -1,9 +1,18 @@
-export const HeroData = {
+export const HERO_DATA = {
   title: 'Eleva tu negocio al',
-  catchTitle: 'siguiente nivel',
+  catch_title: 'siguiente nivel',
   subtitle: 'Todo lo que necesitas para que tus clientes te encuentren',
-  primaryButtonText: 'Trabajemos juntos',
-  primaryButtonLink: '/',
-  secondaryButtonText: 'Proyectos realizados',
-  secondaryButtonLink: '/',
+  primary_button_text: 'Trabajemos juntos',
+  primary_button_link: '/',
+  secondary_button_text: 'Proyectos realizados',
+  secondary_button_link: '/',
+};
+
+export const PORTFOLIO_DATA = {
+  middle_message: 'Una imagen que impacta y una marca que enamora',
+};
+
+export const QUOTE = {
+  text: 'Soluciones web centradas en las',
+  decorated_text: ' necesidades humanas',
 };
--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -24,4 +24,26 @@ export const HEADER = {
 
 export const FOOTER = {
   title: 'Javier miz',
+  text: 'Busco la excelencia desarrollando mis proyectos personales y los tuyos. Me esfuerzo por tener altos estándares de calidad en todo lo que me involucro.',
+  menu_items: [
+    { href: '/', label: 'Inicio' },
+    { href: '/portfolio', label: 'Portfolio' },
+    { href: '/cv', label: 'CV' },
+  ],
+  contact_items: [
+    {
+      href: 'mailto:javiermizarevalo@gmail.com',
+      label: 'javiermizarevalo@gmail.com',
+    },
+    {
+      href: 'https://www.linkedin.com/in/javier-miz/',
+      label: '@javier-miz',
+      target: '_blank',
+    },
+    {
+      href: 'https://www.instagram.com/javiermiz_/',
+      label: '@javiermiz_',
+      target: '_blank',
+    },
+  ],
 };
