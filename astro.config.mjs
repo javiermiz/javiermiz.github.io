8f87dcd555f2023fd477bfe604ecb30ecbc09091
JavierMiz <javiermizarevalo@gmail.com>
2025-01-05 17:12:17 -0500

    mejoras

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://javiermiz.github.io'
+  site: 'https://javiermiz.github.io',
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'en',
+  },
 });
