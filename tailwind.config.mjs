c3d03084d80c1f12ffaf3446fd6af8302c8eab1b
JavierMiz <javiermizarevalo@gmail.com>
2025-01-08 13:27:59 -0500

    remove unused code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,41 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '3rem',
-      },
-    },
     extend: {
-      transitionDuration: {
-        DEFAULT: '300ms',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-      colors: {
-        background: {
-          DEFAULT: '#f7f7f5',
-        },
-        dark: {
-          DEFAULT: '#2B2117',
-        },
-        primary: {
-          DEFAULT: '#2294B1',
-          dark: '#000d6c',
-          light: '#85d5e9',
-        },
-        secondary: {
-          DEFAULT: '#DD34D1',
-        },
-      },
       fontFamily: {
-        primary: ['Saira', 'sans-serif'],
-        sans: ['Inter', 'sans-serif'],
+        primary: ["var(--font-heading)", "sans-serif"],
+        sans: ["var(--font-body)", "sans-serif"],
       },
     },
   },
